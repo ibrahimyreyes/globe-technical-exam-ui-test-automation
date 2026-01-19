b8d656060a7f08f718f29b62c87f181c2c95244e
ibrahimyreyes <ivrahim.reyes@gmail.com>
2026-01-19 17:11:17 +0800

    Update Playwright test timeout settings: increase default action timeout to 60 seconds

--- src/hooks/BaseTest.ts
+++ src/hooks/BaseTest.ts
@@ -14,13 +14,13 @@ export const test = baseTest.extend<HookFixture>({
     // This code runs after all tests
     await context.close();
   },
-
+  
   page: async ({ context }, use) => {
     // This code runs before each test
     //test.setTimeout(0);// No set timeout for each test
     const page = await context.newPage();
     await page.setViewportSize({ width: 1920, height: 1032 });
-    page.setDefaultTimeout(30000); // Set timeout for action (30 seconds)
+    page.setDefaultTimeout(60000); // Set timeout for action (60 seconds)
     page.setDefaultNavigationTimeout(30000); // Set timeout for navigating the page (20 seconds)
     await page.waitForLoadState('networkidle');
     await use(page);
